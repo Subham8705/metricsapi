5adc82e34c254dbf4df4b310ef42bdd8c6d6d577
Subham <subhamkumarshee@gmail.com>
2026-07-08 12:34:31 +0530

    updated public key

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-x948scxi.apps.exam.local"
 
 PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
@@ -40,6 +41,7 @@ WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
 SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
 dQIDAQAB
+-----END PUBLIC KEY-----
 """
 
 # ============================================================
